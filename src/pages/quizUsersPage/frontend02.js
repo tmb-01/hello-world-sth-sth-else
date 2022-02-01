f8cd85634a540fc29cb0b63b066af9a35540b338
Maraim <maraimtuxtasunov@gmail.com>
2022-02-01 17:08:09 +0500

    hello

---
+++ src/pages/quizUsersPage/frontend02.js
@@ -0,0 +1,123 @@
+export const result02 = [
+  {
+      firstName:"Jasurbek",
+      lastName:"Maxmudjonov",
+      result:{
+          html:0,
+          css:0,
+          js:22,
+          react:16,
+          redux:1
+      }
+  },
+  {
+      firstName:"Farrukh",
+      lastName:"Aliev",
+      result: {
+          html:25,
+          css:15,
+          js:41,
+          react:27,
+          redux:7
+      }
+  },
+  {
+      firstName:"Abdullo",
+      lastName:"Nasrullaev",
+      result: {
+          html:26,
+          css:16,
+          js:41,
+          react:24,
+          redux:7
+      }
+  },
+  {
+      firstName:"Almaz",
+      lastName:"Batirbekov",
+      result: {
+          html:21,
+          css:15,
+          js:39,
+          react:21,
+          redux:7
+      }
+  },
+  {
+      firstName:"Akromxon",
+      lastName:"Nimatullayev",
+      result: {
+          html:22,
+          css:17,
+          js:45,
+          react:22,
+          redux:7
+      }
+  },
+  {
+      firstName:"Umid",
+      lastName:"Xamrakulov",
+      result: {
+          html:23,
+          css:16,
+          js:43,
+          react:19,
+          redux:5
+      }
+  },
+  {
+      firstName:"Abdurahmon",
+      lastName:"Sadriddinov",
+      result: {
+          html:23,
+          css:16,
+          js:41,
+          react:0,
+          redux:0
+      }
+  },
+  {
+      firstName:"Abdulatif",
+      lastName:"Abdunazirov",
+      result: {
+          html:22,
+          css:14,
+          js:40,
+          react:22,
+          redux:7
+      }
+  },
+  {
+      firstName:"Sardor",
+      lastName:"Sa'dullaev",
+      result: {
+          html:26,
+          css:14,
+          js:41,
+          react:19,
+          redux:1
+      }
+  },
+  {
+      firstName:"karomatillo",
+      lastName:"haitboev",
+      result: {
+          html:23,
+          css:16,
+          js:40,
+          react:19,
+          redux:5
+      }
+  },
+  {
+      firstName:"Sarvar",
+      lastName:"Zaynobidinov",
+      result: {
+          html:24,
+          css:16,
+          js:40,
+          react:25,
+          redux:8
+      }
+  },
+]
